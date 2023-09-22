df7461e9a94856e2db6173dd91d8841fb3fab9cf
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-09-22 11:10:10 +0200

    Add Contact Form

--- FSJ7/components/contacts/contact-form.js
+++ FSJ7/components/contacts/contact-form.js
@@ -1,14 +1,24 @@
-import classes from ''
+import classes from './contact-form.module.css';
 function ContactForm() {
 return < section className={classes.contact}>
     <h1>How can I help you?</h1>
     <form className={classes.form}>
         <div  className={classes.controls}>
         <div  className={classes.control}>
-            <label>
-                
-            </label>
+            <label htmlFor='email'>Your Email</label>
+            <input type='email' id='mail' required/> 
         </div>
+        <div  className={classes.control}>
+            <label htmlFor='name'>Your Name</label>
+            <input type='text' id='name' required/> 
+        </div>
+        </div>
+        <div  className={classes.control}>
+            <label htmlFor="message">Your Message</label>
+            <textarea id='message' rows='5'></textarea> 
+        </div>
+        <div  className={classes.control}>
+            <button htmlFor="email">Send Message</button>
         </div>
     </form>
 </section>
